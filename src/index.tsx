adfbcfe9ba393bc695dd677db6d29f5d3aaea0db
Hemanth M K <hemanth.mk.ext@ccep.com>
2025-07-30 18:24:03 +0530

    added add function

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,7 @@ import AwesomeLibrary from './NativeAwesomeLibrary';
 export function multiply(a: number, b: number): number {
   return AwesomeLibrary.multiply(a, b);
 }
+
+export function add(a: number, b: number): number {
+  return a+b
+}
